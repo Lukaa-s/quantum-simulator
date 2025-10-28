2a0f1b3b6da244d6a28fea633a8254c99e0bbe35
Guillaume Vauvert <guillaume.vauvert.pro@gmail.com>
2025-10-28 21:19:28 +0000

    gate H OK

--- src/quantumsimulator/register.py
+++ src/quantumsimulator/register.py
@@ -1,61 +1,88 @@
-from dataclasses import dataclass
 import numpy as np
 from quantumsimulator.tensor import Tensor
-I2=np.identity(2)
+
+I2 = np.identity(2)
+
+QubitIndex = int
+
 
 class Register:
-    n_qubits : int
-    
-    def __init__(self, n_qubits : int):
-        self.n_qubits=n_qubits
-        self.values=np.zeros(2**n_qubits,dtype=complex)
-        #FIXME init with what ?
+
+    def __init__(self, n_qubits: int, values: np.ndarray[complex] = np.zeros(0)):
+        self.n_qubits = n_qubits
+        if values.size == 0:
+            self.values = np.zeros(2 ** n_qubits, dtype=complex)
+        else:
+            if values.size != 2 ** n_qubits:
+                raise Exception(f"size: {values.size} != {2 ** n_qubits}")
+            self.values = values
+
+    # FIXME init with what ?
+
+    def set_value(self, idx: QubitIndex, value: complex):
+        self.values[idx] = value
+
+    def get_value(self, idx: QubitIndex) -> complex:
+        return self.values[idx]
 
 
-
-QubitIndex=int
 class Gate1:
     def __init__(self, matrix_values: list[list[complex]]):
-        self.matrix=np.array(matrix_values)
+        self.matrix = np.array(matrix_values)
 
+    @staticmethod
+    def tensor_power(matrix: np.ndarray, n: int) -> np.ndarray:
+        if n == 0:
+            return np.identity(1)
+        if n == 1:
+            return np.identity(np.size(matrix))
+        return np.tensordot(matrix, Gate1.tensor_power(matrix, n - 1))
 
     def apply(self, register: Register, qubit_index: QubitIndex) -> Register:
-        identity_before=Tensor.power(I2,qubit_index)
-        identity_after=Tensor.power(I2,register.n_qubits-qubit_index-1)
-        transform_matrix=np.tensordot(np.tensordot(identity_before,self.matrix),identity_after)
-        return transform_matrix*register
-    
+        identity_before = Gate1.tensor_power(I2, qubit_index)
+        identity_after = Gate1.tensor_power(I2, register.n_qubits - qubit_index - 1)
+        before_dot_matrix = np.kron(identity_before, self.matrix)
+        transform_matrix = np.kron(before_dot_matrix, identity_after)
+        values = transform_matrix.dot(register.values)
+        return Register(register.n_qubits, values)
 
-H=Gate1([[1/np.sqrt(2),1/np.sqrt(2)],
-         [1/np.sqrt(2),-1/np.sqrt(2)]])
+
+SQRT_2 = np.sqrt(2)
+
+H = Gate1([[1 / SQRT_2, 1 / SQRT_2],
+           [1 / SQRT_2, -1 / SQRT_2]])
 
 
 class Gate2:
     def __init__(self, matrix_values: list[list[complex]]):
-        self.matrix=np.array(matrix_values)
-        
+        self.matrix = np.array(matrix_values)
+
     def apply(self, register: Register, qubit_index1: QubitIndex, qubit_index2: QubitIndex) -> Register:
         pass
+
+
 class Gate_n:
     def __init__(self, matrix_values: list[list[complex]]):
-        self.matrix=np.array(matrix_values)
-        
-    def apply(self, register: Register, qubit_indices: list[QubitIndex]) -> Register:
-        u=self.matrix
-        qubit_positions=[k for k in range(register.n_qubits)]
-        swaps={}
-        for i in qubit_indices:
-            if qubit_indices[i]!=i:
-                #swap qubit i and qubit_indices[i]
-                swaps[i]=qubit_indices[qubit_positions[i]]
-                temp=qubit_positions[i]
-                qubit_positions[i]=qubit_positions[qubit_indices[i]] #swap
-                qubit_positions[qubit_indices[i]]=temp  
-        #update matrix
+        self.matrix = np.array(matrix_values)
 
-        return u*register
-        
-        
-        
-        
-    
+    def apply(self, register: Register, qubit_indices: list[QubitIndex]) -> Register:
+        u = self.matrix
+        qubit_positions = [k for k in range(register.n_qubits)]
+        swaps = {}
+        for i in qubit_indices:
+            if qubit_indices[i] != i:
+                # swap qubit i and qubit_indices[i]
+                swaps[i] = qubit_indices[qubit_positions[i]]
+                temp = qubit_positions[i]
+                qubit_positions[i] = qubit_positions[qubit_indices[i]]  # swap
+                qubit_positions[qubit_indices[i]] = temp
+                # update matrix
+
+        return u * register
+
+
+CNOT = Gate2([[1, 0, 0, 0],
+              [0, 1, 0, 0],
+              [0, 0, 0, 1],
+              [0, 0, 1, 0],
+              ])
---
+++ tests/quantumsimulator/test_gate.py
@@ -0,0 +1,40 @@
+import pytest
+
+from quantumsimulator.register import Register, H, SQRT_2, CNOT
+
+
+class TestGate:
+
+    def test_gate_H(self):
+        register_input = Register(1)
+        register_input.set_value(0, 0)
+        register_input.set_value(1, 1)
+        register_output = H.apply(register_input, 0)
+        assert register_output.get_value(0) == 1 / SQRT_2, "wrong H 0 result"
+        assert register_output.get_value(1) == -1 / SQRT_2, "wrong H 1 result"
+
+    def test_gate_CNOT(self):
+        register_input = Register(2)
+        register_input.set_value(0, 0)
+        register_input.set_value(1, 1)
+        register_input.set_value(2, 1)
+        register_input.set_value(3, 0)
+        register_output = CNOT.apply(register_input)
+        assert register_output.get_value(0) == 0, "wrong H 0 result"
+        assert register_output.get_value(1) == 1 + 0j, "wrong H 1 result"
+        assert register_output.get_value(2) == 0, "wrong H 2 result"
+        assert register_output.get_value(3) == 1 + 0j, "wrong H 3 result"
+
+    @pytest.mark.parametrize("input,expected", [
+        ([+1, +1, +1, +1], [+1, +1, +1, +1]),
+        ([+1, -1, +1, -1], [+1, -1, -1, +1]),
+        ([+1, +1, -1, -1], [+1, +1, -1, -1]),
+        ([+1, -1, -1, +1], [+1, -1, +1, -1]),
+    ])
+    def test_gate_CNOT_real(self, input, expected):
+        register_input = Register(2)
+        for idx in range(4):
+            register_input.set_value(idx, input[idx] * 0.5)
+        register_output = CNOT.apply(register_input)
+        for idx in range(4):
+            assert register_output.get_value(idx) == expected[idx] * 0.5, f"wrong CNOT result {idx}"
